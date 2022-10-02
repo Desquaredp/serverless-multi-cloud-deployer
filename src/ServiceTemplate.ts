e1d00156d763fa6caa206dda28977513ff3a6e9d
deep <desquaredp@gmail.com>
2022-10-01 17:46:14 -0700

    basic files for cloud run

---
+++ src/ServiceTemplate.ts
@@ -0,0 +1,20 @@
+import {google} from "@google-cloud/run/build/protos/protos";
+import RevisionTemplate = google.cloud.run.v2.RevisionTemplate;
+
+const container = {
+
+    image: 'gcr.io/hello-world-172023/league@sha256:27b7129fc64c25a6a83fbdaf0614ecdccc985971146dfe58440a2c9bb1c81f51'
+}
+
+const service = {
+    // name: 'projects/hello-world-172023/locations/us-central1/services/league2',
+
+    template: {
+        containers: [container],
+
+    },
+
+}
+
+
+module.exports = service;
